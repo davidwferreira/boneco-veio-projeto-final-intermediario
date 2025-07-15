eeaf196f503548b2e47ec776ff7147d32ac745bb
Anchieta Albano <111232477+Chiet4@users.noreply.github.com>
2025-07-15 17:32:56 -0300

    fix: Reorganizando a ordem dos componentes na página Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
+import HomeSection from '../components/HomeSection/HomeSection'
 import CarouselDestaques from '../components/CarouselDestaques/CarouselDestaques';
 
 const Home = () => {
   return (
     <>
         <Header/>
-        <HomeSection />
         <CarouselDestaques />
+        <HomeSection />
         <Footer/>
     </>
   )
