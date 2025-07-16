2e4267c1377dc468c17f579fd7a8c86bc76bd175
Anchieta Albano <111232477+Chiet4@users.noreply.github.com>
2025-07-16 08:56:06 -0300

    feat: Adicionando nova rota

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import PerfilPage from './pages/PerfilPage.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from './components/Header/Header.jsx';
-import Footer from './components/Footer/Footer.jsx';
 import Prateleira from './components/Prateleira/Prateleira.jsx';
 import CarrinhoPage from './pages/CarrinhoPage.jsx';
+import ProdutoDetalhePage from './components/ProdutoDetalhePage/ProdutoDetalhePage.jsx';
 import './App.css';
 import EditarProduto from './components/EditarProduto/EditarProduto.jsx';
 import CadastrarProduto from './components/CadastrarProduto/CadastrarProduto.jsx'
@@ -17,6 +16,7 @@ function App() {
           <Route path="/perfil" element={<PerfilPage />} />
           <Route path="/" element={<Home />} />
           <Route path="/produtos" element={<Prateleira />} />
+          <Route path="/produto/:id" element={<ProdutoDetalhePage />} />
           <Route path="/produto/editar/:id" element={<EditarProduto />} />
           <Route path="/produto/novo" element={<CadastrarProduto />} />
           <Route path="/carrinho" element={<CarrinhoPage />} />
